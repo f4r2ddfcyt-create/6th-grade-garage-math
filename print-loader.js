6f48d803d55598f0aa54724fb00acf4850a1d4c5
f4r2ddfcyt-create <f4r2ddfcyt@privaterelay.appleid.com>
2026-09-01 07:48:54 -0500

    Refresh printable PDF loader

--- print-loader.js
+++ print-loader.js
@@ -2,7 +2,7 @@
   function loadPrintLibrary(){
     if(document.querySelector('script[data-print-library]')) return;
     const s=document.createElement('script');
-    s.src='print.js?v=20260901-fix1';
+    s.src='print.js?v=20260901-pdf1';
     s.dataset.printLibrary='1';
     document.body.appendChild(s);
   }
